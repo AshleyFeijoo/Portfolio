b81d3977b67211aa20e90ac8edb975f305462988
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-06-11 17:03:09 -0500

    Updated

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,26 @@
 
   $(document).ready(function () {
-      
+
+      $("#modalBtn").click(function (e) { 
+        e.preventDefault();
+        $('#centralModalSuccess').modal();
+        $('#centralModalSuccess').on('hidden.bs.modal', function (e) {
+          e.preventDefault();
+          $("#contactForm").addClass('animated bounceOutLeft');
+          $('#contactForm').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(e) {
+            $("#contactForm").toggle(100, function(){
+              $( "#submittedForm" ).removeClass('d-none');
+              $( "#submittedForm" ).addClass("d-block animated bounceInRight");
+            });
+            // $("#submittedForm").css('display', 'block');
+           
+
+           
+          });
+          
+          })
+
+
+        
+      });
   });
